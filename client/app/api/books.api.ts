385524a3de05292a8ad2cf08e5926bd39305d1b9
adrian-kurnianto <kurnianto.adrian23@gmail.com>
2023-08-14 04:57:17 +0700

    feat: update api for client

--- client/app/api/books.api.ts
+++ client/app/api/books.api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const fetchBooks = async  (keyword: string | undefined) => {
-   const {data} =  await axios.get(`http://localhost:3001/api/v1/search?keyword=${keyword}`)
+   const {data} =  await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/search?keyword=${keyword}`)
    return data
 }
--- client/app/api/wishlist.api.ts
+++ client/app/api/wishlist.api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const addToWishlist =  (payload: Record<string, string>) => {
-   return axios.post(`http://localhost:3001/api/v1/wishlist`, {
+   return axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/wishlist`, {
     ...payload,
   });
 };
 
 export const getWishlist = async () =>{
-  const {data} =  await axios.get(`http://localhost:3001/api/v1/wishlist`)
+  const {data} =  await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/wishlist`)
   return data
 }
